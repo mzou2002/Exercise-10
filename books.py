fb53d8361d20b7f98f88625927bf73c3ca47abcd
derikluc <dluc@terpmail.umd.edu>
2024-11-06 12:22:47 -0500

    update books.py
    needs work still

--- books.py
+++ books.py
@@ -1,4 +1,34 @@
 import sqlite3
+import pandas as pd
 
-def input_data(data_file):
+class Database:
     
+    def __init__(self, csv_file):
+        self.csv_file = csv_file
+        
+    def input_data(self):
+        
+        books = pd.read_csv(self.csv_file)
+        print(books)
+        
+        conn = sqlite3.connect('books.db')
+        cursor = conn.cursor()
+        
+        create = '''CREATE TABLE books (
+                    id INTEGER, title TEXT, author TEXT, year INTEGER
+                    )'''
+        cursor.execute(create)
+        
+        insert = '''INSERT INTO books VALUES (?,?,?,?)'''
+        
+        for index, row in books:
+            cursor.executemany(insert, )
+        
+        read = '''SELECT title, author, year FROM books'''
+        test = cursor.execute(read).fetchall()
+        print(test)
+        
+if __name__ == "__main__":
+    
+    test = Database('books.csv')
+    test.input_data()
